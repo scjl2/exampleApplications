13b6f0b480dd121754bbc8df16abf6324e2ff6ea
matt@wv8544 <ml881@york.ac.uk>
2016-08-09 14:36:40 +0100

    Tweaked Buffer to remove all traces of old waitOnBuffer meth, Andy said it was racey.

--- producerConsumer/Consumer.java
+++ producerConsumer/Consumer.java
@@ -8,41 +8,35 @@ import devices.Console;
 
 public class Consumer extends ManagedThread
 {
-	private final PCMission pcMission;
-	private final Buffer buffer;
+  private final PCMission pcMission;
+  private final Buffer buffer;
 
-	public Consumer(PriorityParameters priority, StorageParameters storage,
-			PCMission pcMission)
-	{
-		super(priority, storage, null);
+  public Consumer(PriorityParameters priority, StorageParameters storage,
+      PCMission pcMission)
+  {
+    super(priority, storage, null);
 
-		this.pcMission = pcMission;
-		buffer = pcMission.getBuffer();
-	}
+    this.pcMission = pcMission;
+    buffer = pcMission.getBuffer();
+  }
 
-	public void run()
-	{
-		Console.println("Consumer!");
+  public void run()
+  {
+    Console.println("Consumer!");
 
-		while (!pcMission.terminationPending())
-		{
-				while (buffer.bufferEmpty("Consumer"))
-				{
-					buffer.waitOnBuffer("Consumer");
-				}
+    while (!pcMission.terminationPending())
+    {
+      int result = 999;
+      try
+      {
+        result = buffer.read();
+      }
+      catch (InterruptedException e)
+      {
+        e.printStackTrace();
+      }
+      Console.println("Consumer Read " + result + " from Buffer");
 
-				int result=999;
-				try
-				{
-					result = buffer.read();
-				}
-				catch (InterruptedException e)
-				{
-					e.printStackTrace();
-				}
-				Console.println("Consumer Read " + result
-						+ " from Buffer");
-
-		}
-	}
+    }
+  }
 }
